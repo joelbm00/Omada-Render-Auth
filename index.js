81c0c289e87cf89c822f2655f7134d39bafc3e87
joelbm00 <146150835+joelbm00@users.noreply.github.com>
2025-06-12 15:17:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 const axios = require('axios');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.post('/omada/auth', async (req, res) => {
-  try {
-    const {
-      clientMac,
-      clientIp,
-      site,
-      vid,
-      t,
-      gatewayMac,
-      redirectUrl
-    } = req.body;
-
-    const authUrl = `http://190.34.133.54/portal/radius/auth?clientMac=${clientMac}&clientIp=${clientIp}&t=${t}&site=${site}&redirectUrl=${encodeURIComponent(redirectUrl)}&gatewayMac=${gatewayMac}&vid=${vid}`;
-
-    const response = await axios.post(authUrl);
-
-    res.status(response.status).send(response.data);
-  } catch (err) {
-    console.error('Error al contactar Omada:', err.message);
-    res.status(500).send('Error al autenticar con el router.');
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor proxy en http://localhost:${PORT}`);
-});
+app.use(bodyParser.json());
